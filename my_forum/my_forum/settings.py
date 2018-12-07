433fcb47a20a3aa2df71ea470b279aefbb72355f
sheepxiaozi <2239798581@qq.com>
2018-12-07 19:49:40 +0800

    创建板块、主题和评论模型类并迁移到MySQL

--- my_forum/my_forum/settings.py
+++ my_forum/my_forum/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'my_forum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',  # 数据库ip
+        'PORT': 3306,  # 数据库端口号
+        'USER': 'root',  # 数据库用户名
+        'PASSWORD': 'mysql',  # 数据库密码
+        # 在mysql中创建数据库forum
+        'NAME': 'forum',  # 数据库名称
     }
 }
 
