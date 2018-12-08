a1d21e9892324588b8d257a77faa974a9ee808c4
sheepxiaozi <2239798581@qq.com>
2018-12-08 16:18:59 +0800

    视图、单元测试和复用模板的基本使用

--- my_forum/boards/tests.py
+++ my_forum/boards/tests.py
@@ -6,14 +6,22 @@ from .views import index, board_topics
 
 
 class IndexTests(TestCase):
-    def test_index_view_status_code(self):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
         url = reverse('index')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
+
+    def test_index_view_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
 
     def test_index_url_resolves_index_view(self):
         view = resolve('/index/')
         self.assertEquals(view.func, index)
+
+    def test_index_view_contains_link_to_topics_page(self):
+        """测试 response 主体是否包含文本 href="/boards/1/"""
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
 
 
 class BoardTopicsTests(TestCase):
@@ -36,6 +44,12 @@ class BoardTopicsTests(TestCase):
         """测试 Django 是否使用了正确的视图函数去渲染 topics"""
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
+
+    def test_board_topics_view_contains_link_back_to_homepage(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('index')
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
 
 
 
