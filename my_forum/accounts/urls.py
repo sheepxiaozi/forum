34d79d07011f264e643f8fbb7ac2a1d7b5fb2f88
sheepxiaozi <2239798581@qq.com>
2018-12-10 19:19:31 +0800

    登录功能的实现

--- my_forum/accounts/urls.py
+++ my_forum/accounts/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
 
