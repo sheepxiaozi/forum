80f00e84dd15c71460f2216ae9c99d9c2c779fc9
sheepxiaozi <2239798581@qq.com>
2018-12-10 16:27:57 +0800

    注册功能的基本实现

--- my_forum/accounts/tests.py
+++ my_forum/accounts/tests.py
@@ -1,3 +1,19 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.urls import reverse
+from .views import signup
 
-# Create your tests here.
+
+class SignUpTests(TestCase):
+    """ 测试状态码（200=success） """
+    def test_signup_status_code(self):
+        url = reverse('signup')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_signup_url_resolves_signup_view(self):
+        """ URL /signup/ 是否返回了正确的视图函数 """
+        view = resolve('/signup/')
+        self.assertEquals(view.func, signup)
+
+
--- my_forum/accounts/views.py
+++ my_forum/accounts/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+# login函数重命名为auth_login以避免与内置login视图冲突
+from django.contrib.auth import login as auth_login
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
