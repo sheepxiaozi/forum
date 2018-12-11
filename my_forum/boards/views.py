c567f93f26a8426e54367d0509af8af03a6647e6
sheepxiaozi <2239798581@qq.com>
2018-12-11 19:31:54 +0800

    发布主题的用户设置为当前的登录用户

--- my_forum/boards/views.py
+++ my_forum/boards/views.py
@@ -18,18 +18,17 @@ def board_topics(request, pk):
 @login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first()  # TODO: get the currently logged in user
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user
             topic.save()
-            post = Comment.objects.create(
+            Comment.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user
             )
             return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
     else:
